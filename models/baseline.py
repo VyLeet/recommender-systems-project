efe7eb9ca2092be84ab9062d1fae7351519b91ae
Maksym Sarana <maksym@verbit.ai>
2023-08-02 16:28:53 +0300

    baseline

--- models/baseline.py
+++ models/baseline.py
@@ -1,5 +1,11 @@
 from abstract_model import AbstractModel
+import numpy as np
 
 
 class BaselineRecommender(AbstractModel):
-    pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        return np.random.randint(1, 5, X.shape[0])
