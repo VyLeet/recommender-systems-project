21f365ee62e887688142e75dc55888d0e1b6d97d
Maksym Sarana <maksym@verbit.ai>
2023-08-02 17:16:52 +0300

    multiple CLI choices

--- run.py
+++ run.py
@@ -9,14 +9,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model', required=True,
                         choices=['base', 'content_based', 'collaborative', 'matrix_factorization'],
-                        help='Model type')
+                        help='Model type', nargs="+")
     script_args = parser.parse_args()
 
     ef = EvaluationFramework()
 
-    if script_args.model == 'base':
-        model_class = BaselineRecommender
-    elif script_args.model == 'matrix_factorization':
-        model_class = MatrixFactorizationRecommender
+    for model_name in script_args.model:
+        if model_name == 'base':
+            model_class = BaselineRecommender
+        elif model_name == 'matrix_factorization':
+            model_class = MatrixFactorizationRecommender
 
-    ef.evaluate(model_class)
+        ef.evaluate(model_class)
