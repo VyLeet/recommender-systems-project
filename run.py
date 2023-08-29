56a9136b3e5050b9c609fc5b9aa7552dfb1fc9f6
VyLeet <n.vysokinskyi@gmail.com>
2023-08-29 14:50:51 +0300

    Add weighted hybrid model

--- run.py
+++ run.py
@@ -3,28 +3,41 @@ from models.matrix_factorization import MatrixFactorizationRecommender
 from models.content_base import ContentBaseRecommender
 from models.random import RandomRecommender
 from models.collaborative import CollaborativeRecommender
+from models.hybrid import HybridRecommender
 import argparse
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', required=True,
-                        choices=['random', 'content_based', 'collaborative', 'matrix_factorization'],
-                        help='Model type', nargs="+")
+    parser.add_argument(
+        "-m",
+        "--model",
+        required=True,
+        choices=[
+            "random",
+            "content_based",
+            "collaborative",
+            "matrix_factorization",
+            "hybrid",
+        ],
+        help="Model type",
+        nargs="+",
+    )
     script_args = parser.parse_args()
 
     ef = EvaluationFramework()
 
     for model_name in script_args.model:
-        if model_name == 'random':
+        if model_name == "random":
             model_class = RandomRecommender
-        elif model_name == 'content_based':
+        elif model_name == "content_based":
             model_class = ContentBaseRecommender
-        elif model_name == 'collaborative':
+        elif model_name == "collaborative":
             model_class = CollaborativeRecommender
-        elif model_name == 'matrix_factorization':
+        elif model_name == "matrix_factorization":
             model_class = MatrixFactorizationRecommender
+        elif model_name == "hybrid":
+            model_class = HybridRecommender
         else:
             raise NotImplementedError()
 
