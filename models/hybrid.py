670fb9c088198ac7f3fbc3ce08acc7ae8b771799
VyLeet <n.vysokinskyi@gmail.com>
2023-08-30 00:48:26 +0300

    Recover weights learning code

--- models/hybrid.py
+++ models/hybrid.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from scipy.optimize import minimize
 from models.abstract_model import AbstractModel
@@ -9,21 +8,37 @@ from models.content_base import ContentBaseRecommender
 from models.matrix_factorization import MatrixFactorizationRecommender
 
 
+# Hybrid weighted ensemble recommender system
 class HybridRecommender(AbstractModel):
     def __init__(self, users, movies):
         super().__init__(users, movies)
 
+        # List the models taking part in ensemble
         self.models: list = [
             CollaborativeRecommender(users, movies),
             ContentBaseRecommender(users, movies),
             MatrixFactorizationRecommender(users, movies),
         ]
 
+        # Initialize the weights equally
         self.weights = np.ones(len(self.models)) / len(self.models)
 
-    def fit(self, X, y):
+    def fit(self, X, y, should_learn_weights=False):
+        # Fit the models
         [model.fit(X, y) for model in self.models]
 
-    def predict(self, X):
+        if should_learn_weights:
+
+            def loss(weights):
+                predictions = self.predict(X, weights)
+                return mean_squared_error(y_val, predictions)
+
+            result = minimize(loss, self.weights, method="BFGS")
+
+            self.weights = result.x
+
+    def predict(self, X, weights=None):
+        if weights is None:
+            weights = self.weights
         predictions = np.array([model.predict(X) for model in self.models]).T
-        return predictions @ np.array(self.weights)
+        return predictions @ weights
