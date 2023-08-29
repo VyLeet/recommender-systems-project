02ac23664d5d739eff1417d1a2f5bc9ba271d02f
Maksym Sarana <maksym@verbit.ai>
2023-08-29 21:12:15 +0300

    train script

--- evaluate.py
+++ evaluate.py
@@ -3,8 +3,10 @@ from tasks.abstract_task import AbstractTask, DATA_DIR
 
 
 class Evaluate(AbstractTask):
-    def __init__(self):
+    def __init__(self, model=None):
         super().__init__(ids_file=DATA_DIR / 'test.ids')
+        if model is not None:
+            self.model = model
 
 
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 from tasks.model_selector import ModelSelector
 from tasks.abstract_task import AbstractTask, DATA_DIR
 import argparse
+from evaluate import Evaluate
 
 
 class Train(AbstractTask):
     def __init__(self, model_cls):
         super().__init__(ids_file=DATA_DIR / 'train.ids')
-        self.model_cls = model_cls
+        self.model = model_cls(users=self.users, movies=self.movies)
 
     def run(self):
         self.model.fit(
@@ -22,7 +23,19 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--model', required=True,
                         choices=ms.get_keys_list(),
                         help='Model type')
+    parser.add_argument('--save', required=False,
+                        type=str, help='Dump model to a file')
+    parser.add_argument('--evaluate', required=False,
+                        type=bool, help='Evaluate model after training', store_true=True)
+
     script_args = parser.parse_args()
 
     train_task = Train(ms[script_args.model])
     train_task.run()
+
+    if script_args.save:
+        train_task.dump_model(script_args.save)
+
+    if script_args.evaluate:
+        evaluate_task = Evaluate()
+        evaluate_task.run()
