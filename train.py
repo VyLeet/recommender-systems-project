e01b691da89a8b05a1584072d47dd317b98653fb
Maksym Sarana <maksym@verbit.ai>
2023-08-29 21:59:34 +0300

    train script

--- train.py
+++ train.py
@@ -5,15 +5,29 @@ from evaluate import Evaluate
 
 
 class Train(AbstractTask):
-    def __init__(self, model_cls):
+    def __init__(self, model_cls, script_arguments):
         super().__init__(ids_file=DATA_DIR / 'train.ids')
-        self.model = model_cls(users=self.users, movies=self.movies)
+
+        parameters = self.get_model_parameters(script_arguments, model_cls)
+        self.model = model_cls(users=self.users, movies=self.movies, **parameters)
 
     def run(self):
         self.model.fit(
             self.ratings.drop(columns='Rating'),
             self.ratings.Rating
         )
+
+    @staticmethod
+    def get_model_parameters(script_arguments, model_cls):
+        parameters = {}
+        cli_key = model_cls.get_cli_key()
+
+        for sn in dir(script_arguments):
+            if sn.startswith(cli_key + '.'):
+                argument = sn.removeprefix(cli_key + '.')
+                parameters[argument] = getattr(script_arguments, sn)
+
+        return parameters
 
 
 if __name__ == '__main__':
@@ -26,16 +40,16 @@ if __name__ == '__main__':
     parser.add_argument('--save', required=False,
                         type=str, help='Dump model to a file')
     parser.add_argument('--evaluate', required=False,
-                        type=bool, help='Evaluate model after training', store_true=True)
+                        help='Evaluate model after training', action='store_true')
 
     script_args = parser.parse_args()
 
-    train_task = Train(ms[script_args.model])
+    train_task = Train(ms[script_args.model], script_args)
     train_task.run()
 
     if script_args.save:
         train_task.dump_model(script_args.save)
 
     if script_args.evaluate:
-        evaluate_task = Evaluate()
+        evaluate_task = Evaluate(model=train_task.model)
         evaluate_task.run()
